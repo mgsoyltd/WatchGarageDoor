af75edbbf1c3f8f3955126092b71c7e148336d6e
Erkki Kauhanen <mgs.ltd@icloud.com>
2020-05-29 17:02:44 +0300

    Version 0.9.8 (2)

--- WatchGarageDoor WatchKit Extension/Views/HomeView.swift
+++ WatchGarageDoor WatchKit Extension/Views/HomeView.swift
@@ -10,61 +10,62 @@ import SwiftUI
 
 struct HomeView: View {
     
-    private enum ImageEnum: String {
-        case shut = "GarageDoorShut"
-        case open = "GarageDoorOpen"
-        case none = ""
-        
-        func next() -> ImageEnum {
-            switch self {
-            case .shut: return .open
-            case .open: return .shut
-            case .none: return .none
-            }
-        }
-        
-        func first() -> ImageEnum {
-            return .shut
-        }
-    }
+//    private enum ImageEnum: String {
+//        case shut = "GarageDoorShut"
+//        case open = "GarageDoorOpen"
+//        case none = ""
+//
+//        func next() -> ImageEnum {
+//            switch self {
+//            case .shut: return .open
+//            case .open: return .shut
+//            case .none: return .none
+//            }
+//        }
+//
+//        func first() -> ImageEnum {
+//            return .shut
+//        }
+//    }
     
-    @State private var fadeOut = false
-    @State private var startApp = false
-    @State private var showAbout = false
-    @State private var timeLeft:CGFloat = 12.0
-    @State private var img = ImageEnum.shut
+    @State private var showAbout = true
+//    @State private var startApp = false
+//    @State private var fadeOut = false
+//    @State private var timeLeft:CGFloat = 12.0
+//    @State private var img = ImageEnum.shut
     
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+//    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         ZStack {
-            Image(img.rawValue)
-                .resizable()
-                .frame(width: 200.0, height: 180.0)
-                .clipped(antialiased: true)
-                .cornerRadius(10.0)
-                .opacity(fadeOut ? 0 : 1)
-                //                .rotation3DEffect(.degrees(25), axis: (x:self.timeLeft * 100.0, y: self.timeLeft * -100.0, z: self.timeLeft * 100.0))
-                .animation(.easeInOut(duration: 1.0))
-                .onReceive(timer) { _ in
-                    if self.timeLeft > 0 {
-                        self.timeLeft -= 1
-                    }
-                    if self.timeLeft > 6 {
-                        self.img = self.img.next()
-                    }
-                    else {
-                        self.img = self.img.first()
-                        self.fadeOut = true
-                        self.showAbout = true
-                    }
-                    if self.timeLeft == 0 {
-                        self.startApp = true
-                    }
-            }
-            .onTapGesture {
-                self.timeLeft = 2
-            }
+//            Image(img.rawValue)
+//                .resizable()
+//                .frame(width: 200.0, height: 180.0)
+//                .clipped(antialiased: true)
+//                .cornerRadius(10.0)
+//                .opacity(fadeOut ? 0 : 1)
+//                //                .rotation3DEffect(.degrees(25), axis: (x:self.timeLeft * 100.0, y: self.timeLeft * -100.0, z: self.timeLeft * 100.0))
+//                .animation(.easeInOut(duration: 1.0))
+//                .onReceive(timer) { _ in
+//                    if self.timeLeft > 0 {
+//                        self.timeLeft -= 1
+//                    }
+//                    if self.timeLeft > 6 {
+//                        self.img = self.img.next()
+//                    }
+//                    else {
+//                        self.img = self.img.first()
+//                        self.fadeOut = true
+//                        self.showAbout = true
+//                    }
+//                    if self.timeLeft == 0 {
+//                        self.startApp = true
+//                    }
+//            }
+//            .onTapGesture {
+//                self.timeLeft = 2
+//            }
+            
             if self.showAbout {
                 Form {
                     AboutView()
